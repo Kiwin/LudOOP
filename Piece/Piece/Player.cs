dac84a125774226fc4776829e22702b01facf313
Kiwin <kikoro@live.dk>
2018-02-13 13:30:17 +0100

    Added Player class Added IOwned interface

---
+++ Piece/Piece/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop
+{
+    /// <summary>
+    /// Class representing a player.
+    /// </summary>
+    public class Player
+    {
+    }
+}
