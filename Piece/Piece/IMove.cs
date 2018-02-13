1ad92e020edd4688a3b63b4d3169ad707465550b
Kiwin <kikoro@live.dk>
2018-02-13 12:35:07 +0100

    Added IMove comments

--- Piece/Piece/IMove.cs
+++ Piece/Piece/IMove.cs
@@ -4,10 +4,28 @@ using System.Text;
 
 namespace Ludoop
 {
+    /// <summary>
+    /// Interface for moving objects around and between maps.
+    /// </summary>
     public interface IMove
     {
+        /// <summary>
+        /// Move a specified amount of tiles.
+        /// </summary>
+        /// <param name="tiles">Amount of tiles to move (Negative amount for backwards).</param>
         void Move(int tiles);
+
+        /// <summary>
+        /// Move to a specified tile.
+        /// </summary>
+        /// <param name="tileIndex">Tile Index</param>
         void MoveTo(int tileIndex);
+
+        /// <summary>
+        /// Move to a specified map and a specified tile.
+        /// </summary>
+        /// <param name="mapIndex">Map index</param>
+        /// <param name="tileIndex">Tile index</param>
         void MoveToMap(int mapIndex, int tileIndex);
     }
 }
