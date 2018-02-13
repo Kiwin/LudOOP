0d3c77262cd68011dc5f2324ddec4a010cd86aa5
Kiwin <kikoro@live.dk>
2018-02-13 12:29:55 +0100

    Added IMove

---
+++ Piece/Piece/IMove.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop
+{
+    public interface IMove
+    {
+        void Move(int tiles);
+        void MoveTo(int tileIndex);
+        void MoveToMap(int mapIndex, int tileIndex);
+    }
+}
