bf2f2b63982aa759cc062928ebe94b743cfc8f48
Kiwin <kikoro@live.dk>
2018-02-14 10:40:33 +0100

    Added IPlayer interface Added ITeam interface Update ITile interface Update Player class Update Piece class Update Tile class

---
+++ Piece/Piece/Backend/ITeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop.Backend
+{
+        public enum PlayerTeam { RED, GREEN, YELLOW, BLUE };
+
+    public interface ITeam
+    {
+
+        PlayerTeam Team{
+            get;
+            set;
+        }
+
+    }
+}
