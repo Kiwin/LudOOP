65bf3e343d72c54139235c0ee163957420d4cd01
Kiwin <kikoro@live.dk>
2018-02-14 10:51:13 +0100

    Added Tiles folder Move ITile to Tiles Move Tile to Tiles

---
+++ Piece/Piece/Backend/Tiles/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop
+{
+    class Tile : ITile
+    {
+        public Tile(int x, int y, TileType type)
+        {
+            this.Position = new Vector2D(x, y);
+            this.Type = type;
+        }
+
+        private Vector2D position;
+        /// <summary>
+        /// get and set, position for object
+        /// </summary>
+        public Vector2D Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        private TileType type;
+
+        /// <summary>
+        /// get and set, type of tile
+        /// </summary>
+        public TileType Type
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+
+        public event EventHandler OnStep;
+        public event EventHandler OnStepEnd;
+
+    }
+}
