8f1631857142548599d3bbebb1c0737ef3cc34c6
Kiwin <kikoro@live.dk>
2018-02-14 11:16:20 +0100

    Added ExitTile class

---
+++ Piece/Piece/Backend/Tiles/ExitTile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop.Backend.Tiles
+{
+    public class ExitTile : Tile
+    {
+
+        public ExitTile(int x, int y, Tile nextMapEntryTile) : base(x, y, TileType.EXIT) {
+            this.nextMapEntryTile = nextMapEntryTile;
+        }
+
+        private Tile nextMapEntryTile;
+
+        public Tile NextMapEntryTile{
+            get { return this.nextMapEntryTile; }
+            set { this.nextMapEntryTile = value; }
+        }
+
+    }
+}
