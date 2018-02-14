fd9ffb1754829dee2b798901bab77653e2ccd746
Kiwin <kikoro@live.dk>
2018-02-14 13:48:02 +0100

    Update MapBuilder class

--- Piece/Piece/Backend/MapBuilder/MapBuilder.cs
+++ Piece/Piece/Backend/MapBuilder/MapBuilder.cs
@@ -5,11 +5,18 @@ using System.Text;
 
 namespace Ludoop.Backend.MapBuilder
 {
+    /// <summary>
+    /// Abstract Class representing a map builder.
+    /// </summary>
     public abstract class MapBuilder
     {
 
+        //List of rules objects.
         protected List<MapBuilderRule> rules;
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public MapBuilder()
         {
             rules = new List<MapBuilderRule>();
@@ -37,45 +44,61 @@ namespace Ludoop.Backend.MapBuilder
 
     }
 
+    /// <summary>
+    /// Class representing a basic map builder.
+    /// </summary>
     public class NormalMapBuilder : MapBuilder
     {
 
-        public NormalMapBuilder()
-        {
+        private TileType defaultTileType;
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="defaultTileType">TileType to fill in when no rule is applies</param>
+        public NormalMapBuilder(TileType defaultTileType = TileType.DEFAULT)
+        {
+            this.defaultTileType = defaultTileType;
         }
 
+        /// <summary>
+        /// Method generates a map based on applied rules.
+        /// </summary>
+        /// <param name="mapSize">Size of the to be generated map.</param>
+        /// <returns></returns>
         public override Map GenerateMap(int mapSize)
         {
-            Map map = new Map(mapSize);
-            for (int i = 0; i < mapSize; i++)
+            Map map = new Map(mapSize); //Instanciate map object.
+            for (int i = 0; i < mapSize; i++) //Loop through tiles.
             {
-                foreach (MapBuilderRule rule in rules)
+                foreach (MapBuilderRule rule in rules) //Loop through rules.
                 {
                     switch (rule.REPEAT_TYPE)
                     {
+                        //If rule should apply only first time 'i' hits interval.
                         case RuleRepetitionType.FIRST:
-                            if (rule.INTERVAL == i)
+                            if (rule.INTERVAL == i) //Check if rule should apply this iteration.
                             {
-                                map.Tiles[i] = new Tile(rule.TILE_TYPE);
-                                break;
+                                map.Tiles[i] = new Tile(rule.TILE_TYPE); //Create new tile
+                                break; //Break out of looping though rules and continue to next tile.
                             }
                             break;
+                        //If rule should apply every time 'i' hits interval.
                         case RuleRepetitionType.EVERY:
-                            if (i % rule.INTERVAL == 0)
+                            if (i % rule.INTERVAL == 0) //Check if rule should apply this iteration.
                             {
-                                map.Tiles[i] = new Tile(rule.TILE_TYPE);
-                                break;
+                                map.Tiles[i] = new Tile(rule.TILE_TYPE); //Create new tile
+                                break; //Break out of looping though rules and continue to next tile.
                             }
                             break;
                     }
                 }
-                if (map.Tiles[i] == null)
+                if (map.Tiles[i] == null) //If no rule were triggered
                 {
-                    map.Tiles[i] = new Tile(TileType.DEFAULT);
+                    map.Tiles[i] = new Tile(defaultTileType); //Set tile to a default tile.
                 }
             }
-            return map;
+            return map; //Return the map.
         }
     }
 
