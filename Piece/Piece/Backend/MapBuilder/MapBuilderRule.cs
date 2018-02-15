f63aed386c3d25f7776e6a9d7a2b5229cd3a7a07
Kiwin <kikoro@live.dk>
2018-02-15 11:18:03 +0100

    Added Actor class Added IDraw interface Update MapBuilder class Update MapBuilderRule class Update Tile class

--- Piece/Piece/Backend/MapBuilder/MapBuilderRule.cs
+++ Piece/Piece/Backend/MapBuilder/MapBuilderRule.cs
@@ -4,20 +4,22 @@ using System.Text;
 
 namespace Ludoop.Backend.MapBuilder
 {
-    public enum RuleRepetitionType { FIRST, EVERY};
+    public enum RuleRepetitionMode { FIRST, EVERY};
 
     public class MapBuilderRule
     {
-        public MapBuilderRule(TileType tileType, RuleRepetitionType repeatType, int interval)
+        public MapBuilderRule(TileType tileType, RuleRepetitionMode repeatType, int interval = 1, int intervalOffset = 0)
         {
             this.TILE_TYPE = tileType;
             this.REPEAT_TYPE = repeatType;
             this.INTERVAL = interval;
+            this.INTERVAL_OFFSET = intervalOffset;
         }
 
         public readonly TileType TILE_TYPE;
-        public readonly RuleRepetitionType REPEAT_TYPE;
+        public readonly RuleRepetitionMode REPEAT_TYPE;
         public readonly int INTERVAL;
+        public readonly int INTERVAL_OFFSET;
 
     }
 }
---
+++ Piece/Piece/View/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop.View
+{
+    public abstract class Actor
+    {
+
+        public abstract void Draw(float x, float y, float w, float h);
+
+    }
+}
