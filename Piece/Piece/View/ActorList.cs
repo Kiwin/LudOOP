bf9c0c089a3e0e7bdc626a3da91c23e106c179b3
Kiwin <kikoro@live.dk>
2018-02-20 11:10:26 +0100

    Update Actor class Added ActorList class Added ConsoleActor class Added TilePlotter class

---
+++ Piece/Piece/View/ActorList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ludoop.View
+{
+    public class ActorList : List<Actor>
+    {
+
+        public ActorList() {
+
+        }
+
+        /// <summary>
+        /// Method for drawing all actors in ActorList.
+        /// </summary>
+        public void DrawAll() {
+            foreach(Actor actor in this) {
+                actor.Draw();
+            }
+        }
+
+    }
+}
